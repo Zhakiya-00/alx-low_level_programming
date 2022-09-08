ff4c2ac20c3d42bffecb7edad0d967627119d2c3
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-09-08 02:40:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
 
+
+
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *   * Return: 0 if success.
+ *   * Return: 0 if exited properly, non-zero otherwise
  *
  *    */
 
@@ -13,8 +15,8 @@ int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }

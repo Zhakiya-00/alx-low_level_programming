b10231e6319211a75d4b7b46aec2f0889441b314
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-09-09 07:28:51 -0700

    yes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
 
 
 /**
- *
  *  * main -Entry point
  *
  *   *
@@ -15,9 +14,10 @@
  *
  *      * Return: Always 0 (Success)
  *
- *       */
+ *      
+ *      */
 
-int main(void)
+int main (void)
 
 {
 

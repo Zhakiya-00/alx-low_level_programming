8938370eb720abe89c1206819a2ca18cc1340a60
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-09-14 05:36:22 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * islower - check if c is lowercase
+ *
+ * Return 1 - if c is lowercase
+ *
+ * Return 0 - if otherwise
+ */
+
+int _islower(int c)
+
+
+	{
+
+		if ( _islower(c) )
+
+		{
+		return (1);
+
+	}
+	else
+	{
+		return (0);
+
+	}
+}

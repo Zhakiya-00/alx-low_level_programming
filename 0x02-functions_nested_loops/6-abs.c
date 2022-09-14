431b5e5b19c6698e44a75c8017ec18b00f66e526
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-09-14 07:59:04 -0700

    absolut

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs - returns absl value n of an int
+ *_abs - returns absl value n of an int
  * @a: parse me
  *
  * Return : the absolute value of int
@@ -11,7 +11,7 @@ int _abs(int a)
 {
 	if (a < 0)
 	{
-		return(-a);
+		return (-a);
 	}
 	else
 	{

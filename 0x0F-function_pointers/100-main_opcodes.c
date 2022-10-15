11493dc663a92b86fd03bd7a31b86c7dbacabebd
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-10-15 14:20:58 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -18,23 +18,23 @@ int main(int argc, char *argv[])
 {
 	int bytes, i;
 	char *arr;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	
+
 	bytes = atoi(argv[1]);
-	
+
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	
+
 	arr = (char *)main;
-	
+
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		
+
 		printf("%02hhx ", arr[i]);
 	}
 	return (0);

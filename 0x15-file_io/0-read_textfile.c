4f36c6af4a3f70009496b270032ab4963fedf50d
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-11-04 11:26:15 -0700

    cp.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
-		return(0);
+		return (0);
 
 	readed = read(fd, buff, letters);
 	write(STDOUT_FILENO, buff, readed);

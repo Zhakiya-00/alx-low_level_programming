110a39dea92adf0677c5f8aeca0fe89b3770b141
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-12-17 11:47:56 -0800

    isupper

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,33 +1,21 @@
 #include "main.h"
 
 /**
-*_isupper - determins if uppercase
-*
-*@c: parameter integer
-
-*
-
-*Return: 1 if uppercase else 0
-*/
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
 
 int _isupper(int c)
 
 {
 
-	if (c >= 65 && c <= 90)
-
-	{
+	if (c >= 'A' && c <= 'Z')
 
 		return (1);
 
-	}
-
 	else
 
-	{
-
 		return (0);
-
-	}
 
 }

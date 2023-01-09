b82aa4b02cd1d32b55280df303450370e136dbd1
zhakiya-00 <zakiyayusuf123@gmail.com>
2023-01-09 02:08:06 -0800

    create

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -31,18 +31,14 @@ void hash_table_print(const hash_table_t *ht)
 
 	{
 		if (ht->array[i] != NULL)
-
 		{
 			if (comma_flag == 1)
 
 				printf(", ");
-
 			node = ht->array[i];
-
 			while (node != NULL)
 
 			{
-
 				printf("'%s': '%s'", node->key, node->value);
 
 				node = node->next;
